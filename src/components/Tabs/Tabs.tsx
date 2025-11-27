e9961719f0885a97964d194c975e541faf5e09ac
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-11-27 15:04:13 -0600

    Index

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -22,9 +22,14 @@ export const Tabs = ({ weeks, selectedWeek, onWeekSelect }: Props) => {
         );
     };
 
+    const selectedIndex = weeks.findIndex(week => week.weekNumber === selectedWeek);
+
     return (
         <div className="w-full px-2 sm:px-0">
-            <TabGroup>
+            <TabGroup
+                selectedIndex={selectedIndex !== -1 ? selectedIndex : 0}
+                onChange={(index) => onWeekSelect(weeks[index].weekNumber)}
+            >
                 <TabList className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
                     {weeks.map((week) => (
                         <Tab
@@ -38,7 +43,6 @@ export const Tabs = ({ weeks, selectedWeek, onWeekSelect }: Props) => {
                                         : 'text-blue-100 hover:bg-white/0.12 hover:text-white'
                                 )
                             }
-                            onClick={() => onWeekSelect(week.weekNumber)}
                         >
                             Semana {week.weekNumber}
                         </Tab>
@@ -53,6 +57,7 @@ export const Tabs = ({ weeks, selectedWeek, onWeekSelect }: Props) => {
                                 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
                             )}
                         >
+                            <h3 className="text-lg font-semibold">Órdenes de la Semana {week.weekNumber}</h3>
                         </TabPanel>
                     ))}
                 </TabPanels>
